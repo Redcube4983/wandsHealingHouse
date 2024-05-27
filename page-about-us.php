2d6dd35829f3ecc00832e268a100f2f534e0a77c
Your Name <you@example.com>
2024-05-27 18:41:28 +0900

    oki

--- page-about-us.php
+++ page-about-us.php
@@ -33,21 +33,21 @@ Template Name: about-us
             <p class="sentence">ワンズヒーリングハウスは、東京・駒込の住宅街にある一軒家。チャネラー・ミハエルノブコが住む家です。常に大天使ミハエル、精霊たち、そして多くの高次元存在があなたの目の前まで降り立ち、見守っています。<br>このハウスを訪れ、チャネリングを体験することで、心身の浄化が起こります。あなたの 身体 - 心 - 魂（スピリット）は整い、本当の安らぎと健康を手に入れることができるでしょう。</p>
             <ul class="img-wrap">
                 <li><img src="<?php echo get_template_directory_uri(); ?>/images/about-us/aboutUs_img01.jpg" alt=""></li>
-                <li><img src="<?php echo get_template_directory_uri(); ?>/images/about-us/aboutUs-img02.jpg" alt=""></li>
-                <li><img src="<?php echo get_template_directory_uri(); ?>/images/about-us/aboutUs-img03.jpg" alt=""></li>
-                <li><img src="<?php echo get_template_directory_uri(); ?>/images/about-us/aboutUs-img04.jpg" alt=""></li>
+                <li><img src="<?php echo get_template_directory_uri(); ?>/images/about-us/aboutUs_img02.jpg" alt=""></li>
+                <li><img src="<?php echo get_template_directory_uri(); ?>/images/about-us/aboutUs_img03.jpg" alt=""></li>
+                <li><img src="<?php echo get_template_directory_uri(); ?>/images/about-us/aboutUs_img04.jpg" alt=""></li>
             </ul>
         </div>
         <ul class="sub-description-area">
             <li>
-                <img src="<?php echo get_template_directory_uri(); ?>/images/about-us/aboutUs-img05" alt="">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/about-us/aboutUs_img05.jpg" alt="">
                 <div class="text-wrap">
                     <h4>「杖」という存在として～「ワンズ」の名前の由来</h4>
                     <p>WAND (ワンド)とは、英語で「杖」<br>”道迷う時に、光へ向かって歩き続けるための支えになる杖”という願いを込めて、ハウスの名前としました。ゆっくりと丁寧に歩むための大切な杖 -WAND として、皆様と一緒に道を進んでいきたいと思っています。</p>
                 </div>
             </li>
             <li>
-                <img src="<?php echo get_template_directory_uri(); ?>/images/about-us/aboutUs-img06" alt="">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/about-us/aboutUs_img06.jpg" alt="">
                 <div class="text-wrap">
                     <h4>チャネリングについて</h4>
                     <p>ワンズヒーリングハウスのセッションでは、大天使ミハエルからのチャネリングメッセージを受け取ることができます。<br>高次元からの深い愛のメッセージは、あなたの魂を癒し、より幸せな人生へと進むエッセンスとなることでしょう。<br>ワンズ ヒーリング ハウスを運営するWANDS JAPAN（ワンズジャパン）の代表・ミハエル ノブコは、大天使ミハエルのオフィシャルチャネラーです。<span>※ワンズ ヒーリング ハウスの活動は、いかなる宗教団体にも属しておらず、関係は一切ございません。</span></p>
@@ -57,7 +57,7 @@ Template Name: about-us
                 </div>
             </li>
             <li>
-                <img src="<?php echo get_template_directory_uri(); ?>/images/about-us/aboutUs-img07" alt="">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/about-us/aboutUs_img07.jpg" alt="">
                 <div class="text-wrap">
                     <h4>WANDS アソシエーション</h4>
                     <p class="profile"><span>ミハエル ノブコ</span><br>
@@ -73,7 +73,7 @@ Template Name: about-us
                 </div>
             </li>
             <li>
-                <img src="<?php echo get_template_directory_uri(); ?>/images/about-us/aboutUs-img08" alt="">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/about-us/aboutUs_img08.jpg" alt="">
                 <div class="text-wrap">
                     <p class="profile"><span>藤原　シュウ</span><br>エグゼクティブ プロデューサー</p>
                     <div class="box">
