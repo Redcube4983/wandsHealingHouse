ac6e613afd66218b661d2105e82f64f38e2eef49
Your Name <you@example.com>
2024-06-06 14:57:46 +0900

    oki

--- footer.php
+++ footer.php
@@ -138,7 +138,7 @@
         $(function(){
         $(".more").on("click", function() {
             $(this).toggleClass("on-click");
-            $(".txt-hide").slideToggle(300);
+            $(this).next(".txt-hide").slideToggle(300);
         });
         }); 
     </script>
