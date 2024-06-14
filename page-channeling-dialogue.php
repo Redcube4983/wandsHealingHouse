5f4e2b954046200c8ccfb599d489f162d5f37c8b
Your Name <you@example.com>
2024-06-14 13:03:26 +0900

    oki

--- page-channeling-dialogue.php
+++ page-channeling-dialogue.php
@@ -27,8 +27,8 @@ Template Name: channeling-dialogue
         <div class="breadcrumbs-area">
             <ul class="breadcrumbs-list">
                 <li class="link"><a href="<?php echo esc_url( home_url( '/') ); ?>">トップ</a></li>
-                <li><a href="<?php echo esc_url( home_url('about-us') ); ?>">ワンズヒーリングハウスについて</a></li>
-                <li>チャネリングについて</li>
+                <li><a href="<?php echo esc_url( home_url('personal-program') ); ?>">パーソナルプログラム</a></li>
+                <li>チャネリングダイアローグ</li>
             </ul>
         </div>
         <div class="contents-area">
@@ -41,12 +41,12 @@ Template Name: channeling-dialogue
                     <div class="content1">
                         <div class="img-wrap">
                             <ul class="right-img-box">
-                                <li><img src="<?php echo get_template_directory_uri(); ?>/images/channeling-dialogue/personalProgram_img01.jpg" alt=""></li>
-                                <li><img src="<?php echo get_template_directory_uri(); ?>/images/channeling-dialogue/personalProgram_img02.jpg" alt=""></li>
+                                <li><img src="<?php echo get_template_directory_uri(); ?>/images/channeling-dialogue/dialogue_img01.jpg" alt=""></li>
+                                <li><img src="<?php echo get_template_directory_uri(); ?>/images/channeling-dialogue/dialogue_img02.jpg" alt=""></li>
                             </ul>
                         </div>
                         <div class="text-wrap">
-                            <h3>チャネリング ダイアローグ<small>（対話）<br>- 個人セッション 45分　対面 または 遠隔（ZOOM）<br>Channeling Dialogue<br>- Personal Session (one to one / online)</small></h3>
+                            <h3>チャネリング ダイアローグ<small>（対話）<br>- 個人セッション 45分　<br class="sp">対面 または 遠隔（ZOOM）<br>Channeling Dialogue<br>- Personal Session (one to one / online)</small></h3>
                             
                             <div class="box">
                                 <p>チャネラー・ミハエルノブコとの１対１の個人セッションです。<br>
@@ -58,11 +58,9 @@ Template Name: channeling-dialogue
                                 <span class="red">※2024年4月より価格・時間を変更いたしました</span>
                                 <div class="price-box">
                                     <div class="price">
-                                        <span>price</span>
+                                        <span>PRICE</span>
                                         <ul>
-                                            <li>60分　13,000円（税込）</li>
-                                            <li>90分　16,000円（税込）</li>
-                                            <li>120分　19,000円（税込）</li>
+                                            <li>5,000円以上の<a href="<?php echo esc_url( home_url('#') ); ?>">ドネーション</a>（寄付・支援金）制</li>
                                         </ul>
                                     </div>
                                     <div class="button-wrap">
@@ -82,7 +80,7 @@ Template Name: channeling-dialogue
                                     <p>大天使ミハエルからのメッセージは、あなたの心の声、魂の声です。<br>
                                     豊かな愛ある人生を歩んでいくためのエッセンス、身の回りで起こっていることの意味、自分の気づかない才能にまで言及し、このハウスでの高いエネルギーとともに癒されていきます。<br><br>チャネリングの際は、チャネラーの意識はそのままの状態で、受け取ったメッセージを直接お伝えいたします。メッセージは、チャネラーの個人的な意見や価値観、思想などは入らず、純粋な言葉だけです。</p>
                                 </div>
-                                <div class="comment">
+                                <div class="michael-comment">
                                     <b>ミハエルノブコからひとこと</b>
                                     <p>私の魂の中に存在するスピリットの名前は、「ミハエル」です。<br class="pc">ハートの中に常にミハエルがいます。<br>そのため、目の前にミハエルが座っているかのように、身近に感じていただけます。<br class="pc">たとえ遠くに離れていても（遠隔）、エネルギーは確実に伝わっていきます。</p>
                                 </div>
@@ -90,7 +88,7 @@ Template Name: channeling-dialogue
                             <li>
                                 <div class="">
                                     <h4>事前にご確認いただきたいこと</h4>
-                                    <ul>
+                                    <ul class="black-point">
                                         <li>遠方にお住まいの方、海外在住の方もZOOMでの遠隔セッションのお申し込みは可能ですが、現在は日本語のみの対応といたしており　ますので、ご了承ください。</li>
                                         <li>カウンセリング内容によっては、所要時間が多少前後する場合がございますので、お時間の余裕を持ってご参加ください。</li>
                                         <li>ご同伴者がいらっしゃる場合は、ご相談ください。</li>
@@ -101,7 +99,7 @@ Template Name: channeling-dialogue
                             <li>
                                 <div class="">
                                     <h4>お支払いについて</h4>
-                                    <ul>
+                                    <ul class="black-point">
                                         <li>当日、セッション終了後に現金またはPayPayにてお支払いください。</li>
                                         <li>ZOOMでの遠隔セッションにご参加の方は、終了後一週間以内にお振込をお願いいたします。</li>
                                     </ul>
@@ -115,7 +113,7 @@ Template Name: channeling-dialogue
                             </li>
                         </ul>
 
-                        <h5>“この家は、ここに集う人たちのみんなの家です”<br>（大天使ミハエル）</h5>
+                        <h5>“この家は、ここに集う人たちの<br class="sp">みんなの家です”<br>（大天使ミハエル）</h5>
                         <p>いただいた代金はすべて、これからのハウスでの活動を広げていくための資金といたします。<br>この家を、皆が自由に参加できる集いの場、そして共同創造（Co-Creation）の場としていくことを目的としています。<br>ご賛同いただけますと幸いです。</p>
                     </div>
                 </div>
