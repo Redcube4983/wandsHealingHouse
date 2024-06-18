fcb7d73f13ea80ee28f302b5529436c9d15d4b5c
Your Name <you@example.com>
2024-06-18 14:50:09 +0900

    oki

--- page-reconnection-therapy.php
+++ page-reconnection-therapy.php
@@ -28,16 +28,18 @@ Template Name: reconnection-therapy
                 <div class="inner-box inner-box1">
                     <h3><span>1.</span>テラスリー温熱療法</h3>
                     <div class="wrap">
-                        <div class="test-box">
+                        <div class="text-wrap">
                             <h5>THERATHREE テラスリー温熱機器</h5>
                             <p>テラヘルツ波と遠赤外線を組み合わせた温熱機器<br>「不健康な細胞やダメージを受けた細胞は冷たい」という日本古来の思想に基づき、鍼灸治療をベースに作られました。<br>70度に全身に温めた面を布の上から当てていきます。火を使いませんので「安全なお灸」といったイメージです。</p>
                         </div>
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/reconnection-therapy/reconnection-therapy_img01.jpg" alt="">
-                        <span class="caption">この金属部分を70度まで温めて、布の上から皮膚に当て、その熱を身体の中に吸収させます。</span>
+                        <div class="img-wrap">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/reconnection-therapy/reconnection-therapy_img01.jpg" alt="">
+                            <span class="caption">この金属部分を70度まで温めて、布の上から皮膚に当て、その熱を身体の中に吸収させます。</span>
+                        </div>
                     </div>
                 </div>
                 <div class="inner-box inner-box2">
-                    <div class="text-box">
+                    <div class="text-wrap">
                         <div class="text1">
                             <h5>テラスリーの効果</h5>
                             <p>自律神経の機能を復活させ、血行やリンパの流れ、気の流れを整えて、自己治癒力や免疫力を高めていきます。見た目はシンプルですが、効果はパワフル。<br>身体の中で不調な部位は臓器は冷えており、その部分の細胞も冷えています。そのためテラスリーを当てた時、その部分だけ熱さを感じることがあります。冷えという自覚がなくても、熱さを感じることで自分の冷えを知ることができるので、自分自身の生活を振り返って改善につなげることができます。<br>お客様の中には、「お腹が温まった」とおっしゃる方が多く、あらためて内臓の冷えに気づいていただけているようです。そして何度か繰り返すことで、次第に細胞の温度は上がって、本来の力を取り戻します。<br>リンパや経絡に沿って施術しますので、老廃物が流れ、筋肉が緩みます。施術後は身体が軽く、お風呂上がりのような血色のよい透明感のある肌になります。</p>
@@ -47,11 +49,11 @@ Template Name: reconnection-therapy
                             <ul class="black-point">
                                 <li>テラヘルツ波は身体に浸透していくまで、６～８時間要しますので、当日の夜は長時間の入浴を避けて、軽めのシャワーかぬるめのお湯をお勧めいたします。</li>
                                 <li>水分補給を十分にしていただくと効果的です。特に白湯や常温のミネラルウォーターなどは、内臓の温かさを保つことができます。</li>
-                                <li class="point-none">-テラスリーは、国際特許を取得している温熱機器であり、施術者はテラスリー施術療法の施術士認定証を取得済みです。（2021年10月）</li>
+                                <li class="point-none">テラスリーは、国際特許を取得している温熱機器であり、施術者はテラスリー施術療法の施術士認定証を取得済みです。（2021年10月）</li>
                             </ul>
                         </div>
                     </div>
-                    <ul>
+                    <ul class="img-wrap">
                         <li><img src="<?php echo get_template_directory_uri(); ?>/images/reconnection-therapy/reconnection-therapy_img02.jpg" alt=""></li>
                         <li><img src="<?php echo get_template_directory_uri(); ?>/images/reconnection-therapy/reconnection-therapy_img03.jpg" alt=""></li>
                     </ul>
@@ -61,13 +63,13 @@ Template Name: reconnection-therapy
                 <div class="inner-box inner-box1">
                     <h3><span>2.</span>オリジナルボディタッチ</h3>
                     <p>ワンズヒーリングハウスのセラピスト、ミハエルノブコは、20年以上に渡り美容業界・スパ業界に携わったスペシャリスト。<br>フランスのリンパドレナージュ、筋肉をほぐすストレスリリース、タイ古式の指圧・ストレッチ、リフレクソロジーなどの様々な技術をお客様の症状に合わせて自由自在に組み合わせ、安全で確実な効果をご提供いたします。</p>
-                    <ul>
+                    <ul class="img-wrap">
                         <li><img src="<?php echo get_template_directory_uri(); ?>/images/reconnection-therapy/reconnection-therapy_img04.jpg" alt=""></li>
                         <li><img src="<?php echo get_template_directory_uri(); ?>/images/reconnection-therapy/reconnection-therapy_img05.jpg" alt=""></li>
                     </ul>
                 </div>
                 <div class="inner-box inner-box2">
-                    <div class="text-box">
+                    <div class="text-wrap">
                         <h5>使用する製品について</h5>
                         <p>デンマーク発　ENDOCA（エンドカ）を使用いたします。CBD製品に特化した、全世界で定評のあるブランドです。<br>国際的なオーガニック認証機関であるECOCERTを取得しています。<br>ワンズヒーリングハウスでは、その安全性と質の高さから、数あるCBD製品の中からENDOCAを選び、ENDOCAのボディ＆フェイシャルオイル、ボディバター、サルブ（バーム）を全身に使用いたします。</p>
                     </div>
@@ -80,11 +82,11 @@ Template Name: reconnection-therapy
                             <li>CBDは、ストレスを取り除き、気持ちを落ち着かせ、特に睡眠障害にも効果を発揮します。<br>疲れと緊張で神経が張り詰めているような方に特に効果を発揮します。身体から力がすっと抜けていくように緩み、穏やかな気持ちに変わっていくのが特徴です。</li>
                         </ul>
                 </div>
-                <span class="asterisk">＊ご希望により、他のオーガニック エッセンシャルオイルを使用する施術に変更することもできます。<br>　カウンセリングの際に、お申し付けください。</span>
+                <span class="asterisk">＊ご希望により、他のオーガニック エッセンシャルオイルを使用する施術に変更することもできます。<br>カウンセリングの際に、お申し付けください。</span>
             </div>
             <div class="content content3">
-                <div class="inner-box inner-box1">
-                    <h3><span>3.</span>チャネリング ダイアローグ（対話）</h3>
+                <div class="inner-box">
+                    <h3><span>3.</span>チャネリング ダイアローグ<br class="sp">（対話）</h3>
                     <div class="inner-box1">
                         <p>施術中にボディを触れながら、ミハエルは様々な身体についてのアドバイスや、心の声をダイレクトに伝えてくれます。施術後のカウンセリングでそのメッセージを深めます。</p>
                         <img src="<?php echo get_template_directory_uri(); ?>/images/reconnection-therapy/reconnection-therapy_img07.jpg" alt="">
